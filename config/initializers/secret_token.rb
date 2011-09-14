1ebad4a53a0b84b4ef5d1c4bf5d96af7a39dc317
Trey Bean <trey@12spokes.com>
2011-09-14 16:35:21 -0600

    Initial commit with Project scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TddiumTest::Application.config.secret_token = '7eaf456db2cd35917f335a655e46dfe81d949b34139621bf13c72ce8d052ca080d6d1b9ad84cec09debf9bec0f9166742df090a0a85b9a4a421c90939071e203'
